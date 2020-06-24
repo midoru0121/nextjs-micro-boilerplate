4dcbb40e6e58cb1a3b28288f52965d60a34440c2
AtaruOhto <ayatuohto@gmail.com>
2020-06-24 15:45:00 +0900

    chore changed example text from backend api

--- api/src/controllers/helloworld/index.ts
+++ api/src/controllers/helloworld/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export const helloWorld = () => {
   return async (_req: Request, res: Response) => {
     try {
-      return res.status(200).send("Hello with Server Side Rendering");
+      return res.status(200).send("Hello with SSR!");
     } catch (e) {
       console.error(e);
       return res.status(500).send();
